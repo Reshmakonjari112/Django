e6a740af17c56b656ad30cbc4816ba041c55868f
Reshmakonjari112 <reshmakonjari112@gmail.com>
2017-11-18 16:20:01 +0530

    Feat: add http404 response

--- music/views.py
+++ music/views.py
@@ -11,15 +11,16 @@ from django.http import HttpResponse
 from .models import Album
 # from django.template import loader
 from django.shortcuts import render
+from django.http import Http404
 
 
 def index(request):
 
     all_albums = Album.objects.all()
     # template = loader.get_template('music/index.html')
-    context = {
-        'all_albums': all_albums,
-    }
+    # context = {
+    #     'all_albums': all_albums,
+    # }
 
     # ===============  Commenting out the HTML code ===============
     # html = ''
@@ -29,8 +30,15 @@ def index(request):
     # ==============================================================
 
     # return HttpResponse(template.render(context, request))
-    return render(request, 'music/index.html', context)
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: " + str(album_id) + " </h2>")
+    # ===============  Commenting out the httpresponse code ===============
+    # return HttpResponse("<h2>Details for Album id: " + str(album_id) + " </h2>")
+    # ==============================================================
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'album': album})
