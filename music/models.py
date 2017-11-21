dabe16130fcb81f35e1a33f07565604112e461af
Reshmakonjari112 <reshmakonjari112@gmail.com>
2017-11-21 18:33:18 +0530

    Feat: add boolean for favorite song

--- music/models.py
+++ music/models.py
@@ -19,5 +19,8 @@ class Song(models.Model):
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
 
+    # adding this line for marking favorite songs
+    is_favorite = models.BooleanField(default=False)
+
     def __str__(self):
         return self.song_title
--- music/urls.py
+++ music/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls import url
 from . import views
 app_name = 'music'
 
+
 urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
@@ -14,5 +15,8 @@ urlpatterns = [
     # /music/712/
     # <------712 is an album_id----------->
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /music/<album_id>/favorite/
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.detail, name='favorite'),
 
 ]
