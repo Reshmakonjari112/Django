3f61af80075facdfd59e907abc19e3cbfc3cf565
Reshmakonjari112 <reshmakonjari112@gmail.com>
2017-11-21 19:34:19 +0530

    Feat: add image for fav song

--- music/views.py
+++ music/views.py
@@ -8,7 +8,7 @@ Created :14 November 2017
 """
 
 from django.http import HttpResponse
-from .models import Album
+from .models import Album, Song
 # from django.template import loader
 from django.shortcuts import render, get_object_or_404
 # from django.http import Http404
@@ -46,3 +46,22 @@ def detail(request, album_id):
     # ==============================================================
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExixt):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'errror_message': 'You did not select a valid, song',
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
+
+
+
+
